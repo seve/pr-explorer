ac066e1ccaf0e048cb8b01dbefeaca97775c6456
Severiano Badajoz <sevebadajoz@gmail.com>
2019-04-03 12:20:41 -0700

    Setup github query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { ApolloProvider, Query } from 'react-apollo';
+
+
+import logo from '../logo.svg';
+import '../App.css';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stupid: '',
+    };
+  }
+
   render() {
+    const { ...state } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Query
+              query={gql`
+                {
+                  nodes(ids:["MDQ6VXNlcjg3MTY4Mjk="]){
+                    ... on User {
+                      email,
+                      login,
+                      id
+                    }
+                  }
+                }
+  `}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error :(</p>;
+
+                return data.nodes.map(({ email, login, id }) => (
+                  <div key={id}>
+                    <p>
+                      {id}
+:
+                      {' '}
+                      {email}
+,
+                      {' '}
+                      {login}
+                    </p>
+                  </div>
+                ));
+              }}
+            </Query>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }

cd161bfb80818c097178f07de41a9ee30338602b
Severiano Badajoz <sevebadajoz@gmail.com>
2019-04-04 21:26:53 -0700

    Create query and fix json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,49 @@ import { ApolloProvider, Query } from 'react-apollo';
 
 import logo from '../logo.svg';
 import '../App.css';
+import jsonData from '../data.json';
+
+const ACCESS_TOKEN = '96ee9c9e301e2d7f79615bbb027228fc6b05c110';
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${ACCESS_TOKEN}`,
+  },
 });
+
+const GET_USER_PRS = gql`
+query GetPR($users: [ID!]!){
+  nodes(ids:$users){
+    ... on User {
+      login,
+      id,
+      pullRequests(orderBy:{field: CREATED_AT, direction: DESC}, first: 20) {
+        nodes{
+          permalink,
+          createdAt
+        }
+      }
+    }
+  }
+}
+`;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    let allIDs = [];
+    jsonData.huddles.forEach((huddle) => {     
+      allIDs = allIDs.concat(huddle.huddle_members.map((user) => {
+
+        return user.node_id;
+      }));
+    });
+
+
     this.state = {
-      stupid: '',
+      allIDs,
     };
   }
 
@@ -28,31 +60,17 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <Query
-              query={gql`
-                {
-                  nodes(ids:["MDQ6VXNlcjg3MTY4Mjk="]){
-                    ... on User {
-                      email,
-                      login,
-                      id
-                    }
-                  }
-                }
-  `}
+              query={GET_USER_PRS}
+              variables={{users: state.allIDs }}
+
             >
               {({ loading, error, data }) => {
                 if (loading) return <p>Loading...</p>;
                 if (error) return <p>Error :(</p>;
 
-                return data.nodes.map(({ email, login, id }) => (
+                return data.nodes.map(({ login, id }) => (
                   <div key={id}>
                     <p>
-                      {id}
-:
-                      {' '}
-                      {email}
-,
-                      {' '}
                       {login}
                     </p>
                   </div>
